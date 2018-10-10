bd97c948904f9be72508290b3af5242899992e76
hthoai <hthoai1006@gmail.com>
2018-10-11 02:34:22 +0700

    OpenList

--- astar.py
+++ astar.py
@@ -1,14 +1,17 @@
-from queue import Queue, PriorityQueue
+from queue import PriorityQueue
 from math import sqrt
 
 
 class Cell:
-    def __init__(self, x, y, g=0, h=0, parent=None):
+    def __init__(self, x=0, y=0, g=0, h=0, parent=None):
         self.x = x
         self.y = y
         self.g = g
         self.h = h
         self.f = g+h
+
+    def set_parent(self, parent):
+        self.parent = parent
 
     def compute_h_f(self, goal):
         self.h = sqrt((goal.x-self.x)**2+(goal.y-self.y)**2)
@@ -18,30 +21,61 @@ class Cell:
         return self.f > other.f
 
 
+grid = [[None for x in range(N)] for x in range(N)]
 start = Cell(0, 0)
 goal = Cell(6, 6)
-N = 7
 
-OpenList = PriorityQueue()
-ClosedList = PriorityQueue()
 
-OpenList.put(start)
+def is_in_bounds(row, col):
+    return (row >= 0) & (row < N) & (col >= 0) & (col < N)
 
-while not OpenList.empty():
-    sx = [-1, 0, 1, 1, 1, 0, -1, -1]
-    sy = [1, 1, 1, 0, -1, -1, -1, 0]
-    q = OpenList.get()
-    successors = []
 
-    for i in range(8):
-        temp = Cell(q.x+sx, q.y+sy)
-        if temp.x >= 0 & temp.x < N & temp.y >= 0 & temp.y < N:
-            successors.append(temp)
+def is_unblocked(grid, row, col):
+    return grid[row][col] == 1
 
-    for s in successors:
-        # Check if this successor is goal
-        if (s.x == goal.x & s.y == goal.y):
-            return
+
+def is_destination(row, col, goal):
+    return row == goal.x & col == goal.y
+
+
+def a_star_search(grid, start, goal):
+    # Create a closed list and initialise it to false
+    # which means that no cell has been included yet
+    ClosedList = [[False for x in range(N)] for x in range(N)]
+    OpenList = PriorityQueue()
+    OpenList.put(start)
+    found_goal = False
+
+    while not OpenList.empty():
+        q = OpenList.get()
+        ClosedList[q.x][q.y] = True
+
+        sx = [-1, 0, 1, 1, 1, 0, -1, -1]
+        sy = [1, 1, 1, 0, -1, -1, -1, 0]
+
+        successors = []
+
+        for i in range(8):
+            temp = Cell(q.x+sx[i], q.y+sy[i])
+            if is_in_bounds(temp.x, temp.y) == True:
+                if is_destination(temp.x, temp.y, goal) == True:
+                    temp.set_parent(q)
+                    print("Found")
+                    found_goal = True
+                    return
+                elif (ClosedList[temp.x][temp.y] == False) & is_unblocked(gird, temp.x, temp.y) == True:
+                    temp.g += 1
+                    temp.compute_h_f(goal)
+
+
+
+                    
+                successors.append(temp)
+
+        for s in successors:
+            # Check if this successor is goal
+            if (s.x == goal.x & s.y == goal.y):
+                return
         else:
             # Compute g (distance between successor and q)
             s.g += 1
